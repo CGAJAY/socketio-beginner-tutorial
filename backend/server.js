ff9f2869383b75bbb666e2ea2aa01481682fe99d
CGAJAY <jmugendi737@gmail.com>
2025-02-21 06:20:42 +0300

    Added Express app to Socket.IO

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import {createServer} from 'http';
 import {Server} from 'socket.io';
+import express from 'express';
+import cors from 'cors';
 
+const app = express(); // Create a new express app
 const PORT = 5000; // Define the port number
 
-// Create a new server
-const httpServer = createServer();
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow requests from the client side
+    credentials: true,
+};
+
+app.use(cors(corsOptions)); // Apply the cors options to the express app
+
+// Create an http server by passing the express app
+const httpServer = createServer(app);
 
 // Create a new instance of socket.io by passing the http server object
 const io = new Server(httpServer, {
@@ -13,6 +23,11 @@ const io = new Server(httpServer, {
         origin: 'http://localhost:3000',
     }
 }); 
+
+// Create a route to handle the root path
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Silence is golden'});
+});
 
 // Listen to the connection event
 io.on('connection', (socket) => {
@@ -36,7 +51,7 @@ io.on('connection', (socket) => {
     });
 });
     
-// Listen to the port 3000
+// Start the server on the specified ports
 httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
